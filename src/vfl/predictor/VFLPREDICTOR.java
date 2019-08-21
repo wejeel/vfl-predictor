2e714eb108bfc333f578391360d87bb6c281ea75
Weje Praise <wejepraise@yahoo.com>
2019-08-21 09:12:11 +0100

    initialize repo

---
+++ src/vfl/predictor/VFLPREDICTOR.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vfl.predictor;
+
+/**
+ *
+ * @author WEJE
+ */
+public class VFLPREDICTOR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Start s = new Start();
+        s.setVisible(true);
+    }
+    
+}
